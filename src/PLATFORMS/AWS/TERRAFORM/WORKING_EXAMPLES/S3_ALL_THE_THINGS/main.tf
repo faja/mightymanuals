f58847ec92a5865fb64fb354248e17f8d801aed5
faja <mcabaj@gmail.com>
2023-04-06 23:45:53 +0200

    update aws tf s3

--- src/PLATFORMS/AWS/TERRAFORM/WORKING_EXAMPLES/S3_ALL_THE_THINGS/main.tf
+++ src/PLATFORMS/AWS/TERRAFORM/WORKING_EXAMPLES/S3_ALL_THE_THINGS/main.tf
@@ -18,6 +18,16 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
 resource "aws_s3_bucket_acl" "main" {
   bucket = aws_s3_bucket.main.id

673fd11c7be4e4e122b65df44de9f6455f4e2c19
faja <mcabaj@gmail.com>
2023-04-07 00:16:03 +0200

    one more aws tf s3

--- src/PLATFORMS/AWS/TERRAFORM/WORKING_EXAMPLES/S3_ALL_THE_THINGS/main_encryption.tf
+++ src/PLATFORMS/AWS/TERRAFORM/WORKING_EXAMPLES/S3_ALL_THE_THINGS/main_encryption.tf
@@ -1,31 +1,30 @@
 // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_server_side_encryption_configuration
 // https://docs.aws.amazon.com/AmazonS3/latest/userguide/serv-side-encryption.html
 
-// encrypt with SSE-S3
-server_side_encryption_configuration {
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.main.id
+
+  // encrypt with SSE-S3
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
-}
 
-// encrypt with KMS aws/s3 key
-server_side_encryption_configuration {
+  // OR encrypt with KMS aws/s3 key
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
       kms_master_key_id = "aws/s3"
     }
   }
-}
 
-// encrypt with own KMS key
-server_side_encryption_configuration {
+  // OR encrypt with own KMS key
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
       kms_master_key_id = key_id
     }
   }
+
 }
